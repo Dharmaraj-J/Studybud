e76188192de60fa37a03a7cf4aa166028298c05e
Dharmarj-J <dharmaraj200303@gmail.com>
2023-07-16 12:55:35 -1200

    updated

--- base/views.py
+++ base/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+rooms = [
+    {'id':1, 'name':'let learn python'},
+    {'id':2, 'name':'Desingn with me'},
+    {'id':3, 'name':'Frontend Developers'},
+]
+
+def home(request):
+    return render(request,'home.html',{'rooms':rooms})
+
+def room(request):
+    return render(request,'room.html')
